bf1bb7518013f0d43537711ea5d7d0155cf90d9c
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2021-07-02 21:15:29 +0200

    init auth service

---
+++ auth/src/main/java/com/gokul/chatsappauth/ChatsAppAuthApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.chatsappauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsAppAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatsAppAuthApplication.class, args);
+    }
+
+}
---
+++ auth/src/test/java/com/gokul/chatsappauth/ChatsAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.chatsappauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsAppAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
